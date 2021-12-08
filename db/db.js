8e5d751636f487bc3d7708be520428f2f44ee4a7
RashaSaud <Rasha.Saud.rae.js@tuwaiq.edu.sa>
2021-12-08 14:27:27 +0300

    todolist

---
+++ db/db.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URL).then(
+  () => {
+    console.log("DB connected");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
